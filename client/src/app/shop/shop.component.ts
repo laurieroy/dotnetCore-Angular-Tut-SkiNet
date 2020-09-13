b141dad92e05df9db2a2f232389262af0fdbbd5f
Laurie Roy <laurieroy.dev@gmail.com>
2020-09-13 10:36:00 -0600

    Add reset page number, don't fire PageChanged on filter

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -60,6 +60,7 @@ export class ShopComponent implements OnInit {
 
   onBrandSelected(brandId: number) {
     this.shopParams.brandId = brandId;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
@@ -70,16 +71,20 @@ export class ShopComponent implements OnInit {
 
   onTypeSelected(typeId: number) {
     this.shopParams.typeId = typeId;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
   onPageChanged(event: any) {
-    this.shopParams.pageNumber = event;
-    this.getProducts();
+    if (this.shopParams.pageNumber !== event) {
+      this.shopParams.pageNumber = event;
+      this.getProducts();
+    }
   }
 
   onSearch() {
     this.shopParams.search = this.searchTerm.nativeElement.value;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
