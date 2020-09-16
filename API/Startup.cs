b338237192fe73fc14e30b01f0b22eb5941c07df
Laurie Roy <laurieroy.dev@gmail.com>
2020-09-16 10:13:12 -0600

    Add Redis

--- API/Startup.cs
+++ API/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
+using StackExchange.Redis;
 
 namespace API
 {
@@ -27,6 +28,11 @@ namespace API
             services.AddControllers ();
             services.AddDbContext<StoreContext> (x =>
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+            services.AddSingleton<ConnectionMultiplexer>(c => {
+                var configuration = ConfigurationOptions.Parse(_config
+                    .GetConnectionString("Redis"), true);
+                return ConnectionMultiplexer.Connect(configuration);
+            });
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
             services.AddCors(opt =>
