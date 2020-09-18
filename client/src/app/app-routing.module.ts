4d25f52f5fb1aba75ac39dedf64f929b6176ed21
Laurie Roy <laurieroy.dev@gmail.com>
2020-09-18 17:32:30 -0600

    Add Basket mod, svc, routing, component. i uuid

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -13,6 +13,8 @@ const routes: Routes = [
   {path: 'server-error', component: ServerErrorComponent, data: {breadcrumb: 'Server Error'}},
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule),
     data: {breadcrumb: 'Shop'}},
+  {path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule),
+    data: {breadcrumb: 'Basket'}},
   {path: '**', redirectTo: 'not-found', pathMatch: 'full'}
 ];
 
