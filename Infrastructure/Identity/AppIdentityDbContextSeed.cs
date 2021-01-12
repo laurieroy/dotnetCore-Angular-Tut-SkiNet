b170c7129eb909de60788319764350463d3cad96
Laurie Roy <laurieroy.dev@gmail.com>
2021-01-12 15:04:14 -0700

    Set up Identity

---
+++ Infrastructure/Identity/AppIdentityDbContextSeed.cs
@@ -0,0 +1,34 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Identity
+{
+    public class AppIdentityDbContextSeed
+    {
+        public static async Task SeedUsersAsync(UserManager<AppUser> userManger)
+        {
+            if (!userManger.Users.Any())
+            {
+                var user = new AppUser
+                {
+                    DisplayName = "Bob",
+                    Email = "bob@test.com",
+                    UserName = "bob@test.com",
+                    Address = new Address
+                    {
+                        FirstName = "Bob",
+                        LastName = "Bobbity",
+                        Street = "10 The Street",
+                        City = "Portland",
+                        State = "OR",
+                        Zipcode = "97211"
+                    }
+                };
+
+                await userManger.CreateAsync(user, "Pa$$w0rd");
+            }
+        }
+    }
+}
