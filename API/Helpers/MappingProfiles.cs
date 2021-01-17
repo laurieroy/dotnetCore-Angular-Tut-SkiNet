3cc57c48b82abab575c4173598c52457066999df
Laurie Roy <laurieroy.dev@gmail.com>
2021-01-17 13:19:16 -0700

    Add UserManger Extension methods

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -1,6 +1,7 @@
 using API.DTOs;
 using AutoMapper;
 using Core.Entities;
+using Core.Entities.Identity;
 
 namespace API.Helpers
 {
@@ -11,7 +12,10 @@ namespace API.Helpers
             CreateMap<Product, ProductToReturnDto>()
                 .ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name))
                 .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
-                .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>());   
+                .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
+            
+            CreateMap<Address, AddressDto>().ReverseMap();
+       
         }
     }
 }
